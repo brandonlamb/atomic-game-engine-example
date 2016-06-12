ce748d1c448c9302e0af1844a8fd982b0091b3d1
Brandon Lamb <brandon@brandonlamb.com>
2016-06-12 10:58:31 -0700

    code cleanup

--- Resources/Components/SpaceGame/Characters/Player.js
+++ Resources/Components/SpaceGame/Characters/Player.js
@@ -1,20 +1,21 @@
+'atomic component';
 "use strict";
 var __extends = (this && this.__extends) || function (d, b) {
     for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
     function __() { this.constructor = d; }
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
-'atomic component';
 var Console_1 = require("../../../Modules/Atomic/Console");
 var Player = (function (_super) {
     __extends(Player, _super);
     function Player() {
         _super.apply(this, arguments);
-        this.inspectionFields = {
+        this.inspectorFields = {
             health: [Atomic.VAR_INT, 0],
             armor: [Atomic.VAR_INT, 0],
             allowMove: [Atomic.VAR_BOOL, true],
-            allowShoot: [Atomic.VAR_BOOL, true]
+            allowShoot: [Atomic.VAR_BOOL, true],
+            allowZoom: [Atomic.VAR_BOOL, true]
         };
         this.debug = Console_1.default.debug;
         this.game = Atomic.game;
@@ -22,7 +23,8 @@ var Player = (function (_super) {
         this.shootDelta = 0;
         this.delta = 0;
         this.allowMove = true;
-        this.allowShoot = false;
+        this.allowShoot = true;
+        this.allowZoom = true;
         this.health = 10;
         this.armor = 10;
     }
@@ -32,16 +34,6 @@ var Player = (function (_super) {
         var sprite2D = this.node.createComponent('StaticSprite2D');
         sprite2D.sprite = spaceSheet.getSprite('spaceship_mantis');
         sprite2D.blendMode = Atomic.BLEND_ALPHA;
-        var height = this.game.graphics.height;
-        var half = height * Atomic.PIXEL_SIZE * .5;
-        var y = 0;
-        Atomic.print('height=' + this.game.graphics.height, ', pixel_size=' + Atomic.PIXEL_SIZE, ', half=' + half, ', y=' + y);
-        var camera = this.game.camera.node;
-        var pos1 = camera.getWorldPosition2D();
-        var pos2 = camera.getPosition2D();
-        Atomic.print('worldX=', pos1[0], ', worldY=', pos1[1]);
-        Atomic.print('cameraX=', pos2[0], ', cameraY=', pos2[1]);
-        this.node.position2D = [0, y];
         this.spawnPosition = this.node.getPosition2D();
         this.movement = {
             cursor: {
@@ -73,12 +65,13 @@ var Player = (function (_super) {
             return;
         }
         var i = this.input;
-        if (!i.getKeyDown(Atomic.KEY_W) && !i.getKeyDown(Atomic.KEY_UP) && !i.getKeyDown(Atomic.KEY_SPACE)) {
+        if (!i.getKeyDown(Atomic.KEY_SPACE)) {
             return;
         }
         this.shootDelta = 0.15;
         var pos = this.node.position2D;
         pos[1] += .5;
+        Atomic.print('SHOOT');
     };
     Player.prototype.moveShip = function (timeStep) {
         var config = this.game.config;
@@ -87,10 +80,7 @@ var Player = (function (_super) {
         var speed = 2.0 * timeStep;
         var rotationSpeed = 0.4;
         var pos = this.node.position2D;
-        var pos2 = this.node.position2D;
         var rot = this.node.getRotation2D();
-        var rightPos;
-        var leftPos;
         var i = this.input;
         var left = i.getKeyDown(Atomic.KEY_LEFT) || i.getKeyDown(Atomic.KEY_A);
         var right = i.getKeyDown(Atomic.KEY_RIGHT) || i.getKeyDown(Atomic.KEY_D);
@@ -137,15 +127,21 @@ var Player = (function (_super) {
         if (this.allowMove) {
             this.moveShip(timeStep);
         }
+        if (this.allowZoom) {
+            this.zoom(timeStep);
+        }
     };
-    Player.prototype.clamp = function (value, min, max) {
-        if (value < min) {
-            return min;
+    Player.prototype.zoom = function (timeStep) {
+        var camera = this.game.camera;
+        var i = this.input;
+        var zoomIn = i.getKeyDown(Atomic.KEY_I);
+        var zoomOut = i.getKeyDown(Atomic.KEY_O);
+        if (zoomIn) {
+            camera.zoom += timeStep;
         }
-        else if (value > max) {
-            return max;
+        if (zoomOut) {
+            camera.zoom -= timeStep;
         }
-        return value;
     };
     return Player;
 }(Atomic.JSComponent));
--- src/Resources/Components/SpaceGame/Characters/Player.ts
+++ src/Resources/Components/SpaceGame/Characters/Player.ts
@@ -1,16 +1,16 @@
-import Vector2 = Atomic.Vector2;
 'atomic component';
 
+import Vector2 = Atomic.Vector2;
 import SpaceGame from "../../../Modules/SpaceGame/SpaceGame";
 import Console from "../../../Modules/Atomic/Console";
-import GameConfig from "../../../Modules/Atomic/GameConfig";
 
 class Player extends Atomic.JSComponent {
-  inspectionFields = {
+  inspectorFields = {
     health: [Atomic.VAR_INT, 0],
     armor: [Atomic.VAR_INT, 0],
     allowMove: [Atomic.VAR_BOOL, true],
-    allowShoot: [Atomic.VAR_BOOL, true]
+    allowShoot: [Atomic.VAR_BOOL, true],
+    allowZoom: [Atomic.VAR_BOOL, true]
   };
 
   private debug:Function = Console.debug;
@@ -23,10 +23,16 @@ class Player extends Atomic.JSComponent {
   // private lastUpdate:number;
 
   allowMove:boolean = true;
-  allowShoot:boolean = false;
+  allowShoot:boolean = true;
+  allowZoom:boolean = true;
   health:number = 10;
   armor:number = 10;
 
+  /**
+   * @function Player:start
+   * @inheritDoc
+   * @override
+   */
   start():void {
     // this.lastUpdate = Date.now();
     this.input = this.game.input;
@@ -36,32 +42,6 @@ class Player extends Atomic.JSComponent {
     sprite2D.sprite = spaceSheet.getSprite('spaceship_mantis');
     sprite2D.blendMode = Atomic.BLEND_ALPHA;
 
-    let height = this.game.graphics.height;
-
-    // let half = -height + 1.0;
-    // let half = -(height * Atomic.PIXEL_SIZE);
-    let half = height * Atomic.PIXEL_SIZE * .5;
-    // let half = height * .5;
-    // let half = height / 2;
-
-    // let y = height - 100;
-    // let y = -half + .65;
-    let y = 0;
-
-    Atomic.print(
-      'height=' + this.game.graphics.height,
-      ', pixel_size=' + Atomic.PIXEL_SIZE,
-      ', half=' + half,
-      ', y=' + y
-    );
-
-    let camera = this.game.camera.node;
-    let pos1:Atomic.Vector2 = camera.getWorldPosition2D();
-    let pos2:Atomic.Vector2 = camera.getPosition2D();
-    Atomic.print('worldX=', pos1[0], ', worldY=', pos1[1]);
-    Atomic.print('cameraX=', pos2[0], ', cameraY=', pos2[1]);
-
-    this.node.position2D = [0, y];
     this.spawnPosition = this.node.getPosition2D();
 
     this.movement = {
@@ -78,6 +58,10 @@ class Player extends Atomic.JSComponent {
     }
   }
 
+  /**
+   * Handle player hit
+   * @function Player:onHit
+   */
   onHit():void {
     Atomic.print('Player.onHit()');
 
@@ -109,61 +93,35 @@ class Player extends Atomic.JSComponent {
     }
 
     const i:Atomic.Input = this.input;
-    if (!i.getKeyDown(Atomic.KEY_W) && !i.getKeyDown(Atomic.KEY_UP) && !i.getKeyDown(Atomic.KEY_SPACE)) {
+    if (!i.getKeyDown(Atomic.KEY_SPACE)) {
       return;
     }
-    // if (!this.input.getKeyDown(Atomic.KEY_W) && !this.input.getKeyDown(Atomic.KEY_UP) && !this.input.getKeyDown(Atomic.KEY_SPACE)) {
-    //   return;
-    // }
 
     this.shootDelta = 0.15;
 
     let pos = this.node.position2D;
     pos[1] += .5;
 
+    Atomic.print('SHOOT');
+
     // SpaceGame.spawnBullet(pos, true);
   }
 
   moveShip(timeStep:number):void {
-    let config:GameConfig = this.game.config;
-    let camera:Atomic.Camera = <Atomic.Camera>this.game.camera;
+    let config = this.game.config;
+    let camera = <Atomic.Camera>this.game.camera;
     let cameraNode:Atomic.Node = <Atomic.Node>camera.getNode();
-    let speed:number = 2.0 * timeStep;
-    let rotationSpeed:number = 0.4;
-    let pos:Atomic.Vector2 = this.node.position2D;
-    let pos2:Atomic.Vector2 = this.node.position2D;
-    let rot:number = this.node.getRotation2D();
-    let rightPos:number;
-    let leftPos:number;
+    let speed = 2.0 * timeStep;
+    let rotationSpeed = 0.4;
+    let pos = this.node.position2D;
+    let rot = this.node.getRotation2D();
 
-    const i:Atomic.Input = this.input;
-    let left:boolean = i.getKeyDown(Atomic.KEY_LEFT) || i.getKeyDown(Atomic.KEY_A);
-    let right:boolean = i.getKeyDown(Atomic.KEY_RIGHT) || i.getKeyDown(Atomic.KEY_D);
-    let up:boolean = i.getKeyDown(Atomic.KEY_UP) || i.getKeyDown(Atomic.KEY_W);
-    let down:boolean = i.getKeyDown(Atomic.KEY_DOWN) || i.getKeyDown(Atomic.KEY_S);
-    let shoot:boolean = i.getKeyDown(Atomic.KEY_SPACE);
-    // let jump:boolean = i.getKeyDown(Atomic.KEY_UP) || i.getKeyDown(Atomic.KEY_SPACE) || i.getKeyDown(Atomic.KEY_W);
-
-    // left/right to move
-    /*
-     if (left) {
-     leftPos = pos[0] - speed;
-     // if (leftPos >= -this.game.halfWidth + 1) {
-     if (leftPos >= -config.levelWidth * Atomic.PIXEL_SIZE) {
-     pos[0] = leftPos;
-     camera.translate2D([-Atomic.PIXEL_SIZE, 0]);
-     }
-     }
-
-     if (right) {
-     rightPos = pos[0] + speed;
-     // if (rightPos <= this.game.halfWidth - 1) {
-     if (rightPos <= config.levelWidth * Atomic.PIXEL_SIZE) {
-     pos[0] = rightPos;
-     camera.translate2D([Atomic.PIXEL_SIZE, 0]);
-     }
-     }
-     */
+    const i = this.input;
+    let left = i.getKeyDown(Atomic.KEY_LEFT) || i.getKeyDown(Atomic.KEY_A);
+    let right = i.getKeyDown(Atomic.KEY_RIGHT) || i.getKeyDown(Atomic.KEY_D);
+    let up = i.getKeyDown(Atomic.KEY_UP) || i.getKeyDown(Atomic.KEY_W);
+    let down = i.getKeyDown(Atomic.KEY_DOWN) || i.getKeyDown(Atomic.KEY_S);
+    let shoot = i.getKeyDown(Atomic.KEY_SPACE);
 
     // left/right to rotate
     if (left) {
@@ -176,44 +134,18 @@ class Player extends Atomic.JSComponent {
 
     // Up for thrust
     if (up) {
-      // pos[0] += speed * Math.cos(rot * Math.PI / 180);
-      // pos[1] += speed * Math.sin(rot * Math.PI / 180);
-
       let levelX = config.levelWidth * Atomic.PIXEL_SIZE;
       let levelY = config.levelHeight * Atomic.PIXEL_SIZE;
       let x = pos[0] + speed * Math.cos(rot * Math.PI / 180);
       let y = pos[1] + speed * Math.sin(rot * Math.PI / 180);
 
-      // let camX = pos2[0] + speed * Math.cos(rot * Math.PI / 180);
-      // let camY = pos2[1] + Math.sin(rot * Math.PI / 180);
-
       if (x >= -levelX && x <= levelX) {
         pos[0] = x;
-        // cameraNode.translate2D([pos[0], pos[1] - 0.9893]);
-        // cameraNode.setPosition2D([pos[0], pos[1] - 0.9893]);
-        // cameraNode.setPosition2D([camX / 2, 0]);
-        // camera.setZoom(1);
       }
 
       if (y >= -levelY && y <= levelY) {
         pos[1] = y;
-        // camera.translate2D([x * Atomic.PIXEL_SIZE, 0]);
       }
-
-      // camera.translate2D([pos[0] * Atomic.PIXEL_SIZE, pos[1] * Atomic.PIXEL_SIZE]);
-
-      // Clamp the camera position to the world bounds while centering the camera around the player
-      // let camX = this.clamp(-player.x + canvas.width / 2, yourWorld.minX, yourWorld.maxX - canvas.width);
-      // let camY = this.clamp(-player.y + canvas.height / 2, yourWorld.minY, yourWorld.maxY - canvas.height);
-
-      // let viewport:Atomic.Viewport = this.game.viewport;
-      // let camX = this.clamp(-pos[0] + viewport.getWidth() / 2, -levelX, levelX - viewport.getWidth());
-      // let camY = this.clamp(-pos[1] + viewport.getHeight() / 2, -levelY, levelY - viewport.getHeight());
-      // Atomic.print(`\nCAMX=${camX}\nCAMY=${camY}\nPOSX=${pos[0]}\nPOSY=${pos[1]}`);
-
-      // camera.setPosition2D([-camX, camY]);
-      // camera.translate2D(camX, camY);
-      // camera.translate2D(pos[0] * Atomic.PIXEL_SIZE, 0);
     }
 
     // Up for thrust
@@ -232,27 +164,16 @@ class Player extends Atomic.JSComponent {
       }
     }
 
-    // this.node.position2D = pos;
-    // this.node.setPosition2D(pos);
     this.node.position2D = pos;
-    // this.node.translate2D(pos);
-    // cameraNode.position = this.node.position;
-    // cameraNode.setPosition2D(pos);
     cameraNode.position2D = pos;
-
-    // pos.y += -0.1;
-    // camera.setPosition2D(pos);
-    // camera.translate2D([pos[0] * Atomic.PIXEL_SIZE, pos[1] * Atomic.PIXEL_SIZE]);
-
-    // let pos1:Atomic.Vector2 = camera.getWorldPosition2D();
-    // let pos2:Atomic.Vector2 = camera.getPosition2D();
-    // Atomic.print('worldX=', pos1[0], ', worldY=', pos1[1]);
-    // Atomic.print('cameraX=', pos2[0], ', cameraY=', pos2[1]);
-
-    // camera.setPosition2D(pos);
-    // camera.translate2D(2, 0);
+    // cameraNode.position = [pos[0], pos[1], cameraNode.position[2]];
   }
 
+  /**
+   * @function Player:update
+   * @inheritDoc
+   * @override
+   */
   update(timeStep:number):void {
     if (this.allowShoot) {
       this.doShooting(timeStep);
@@ -261,16 +182,30 @@ class Player extends Atomic.JSComponent {
     if (this.allowMove) {
       this.moveShip(timeStep);
     }
+
+    if (this.allowZoom) {
+      this.zoom(timeStep);
+    }
   }
 
-  private clamp(value:number, min:number, max:number):number {
-    if (value < min) {
-      return min;
-    } else if (value > max) {
-      return max;
+  /**
+   * Zoom the camera in or out
+   * @function Player:zoom
+   * @param {number} timeStep
+   */
+  private zoom(timeStep:number):void {
+    const camera = <Atomic.Camera>this.game.camera;
+    const i = this.input;
+    const zoomIn = i.getKeyDown(Atomic.KEY_I);
+    const zoomOut = i.getKeyDown(Atomic.KEY_O);
+
+    if (zoomIn) {
+      camera.zoom += timeStep;
     }
 
-    return value;
+    if (zoomOut) {
+      camera.zoom -= timeStep;
+    }
   }
 }
 
